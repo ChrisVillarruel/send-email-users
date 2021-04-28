3187dbba44c6e939774cfc5c6b41afb5d0f3759e
chris_villarruel <givc980909@gmail.com>
2021-04-28 14:04:19 -0500

    No se añadio ningun cambio en views de la app user

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework import status
+
+# Modulos locales
+from .serializers import LoginSerializer
+
+class LoginAPIView(CreateAPIView):
+    serializer_class = LoginSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
