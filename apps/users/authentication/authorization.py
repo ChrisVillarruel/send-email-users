5fb44b9c21fd93a290284453cc3949234e1643bc
chris_villarruel <givc980909@gmail.com>
2021-04-29 14:22:28 -0500

    No se añdio ningun cambio dentro del modulo authorization

--- apps/users/authentication/authorization.py
+++ apps/users/authentication/authorization.py
@@ -1,7 +1,7 @@
 # Modulos de django
 from django.conf import settings
 
-# Modulos de rest_framework 
+# Modulos de rest_framework
 from rest_framework.authentication import BaseAuthentication, get_authorization_header
 from rest_framework.exceptions import AuthenticationFailed
 
@@ -23,10 +23,10 @@ class JWTAuthentication(BaseAuthentication):
         auth_prefix_header = self.prefix_header_authentication.lower()
 
         if not auth_header:
-            """ 
+            """
             Si la cabecera de autenticación es false, quiere decir que ningun usuario esta autenticado.
             retornamos None, pues el inicio de sesión no requeire de un token de acceso
-            
+
 
             """
 
@@ -47,7 +47,7 @@ class JWTAuthentication(BaseAuthentication):
         jwt_token = auth_header[1].decode('utf-8')
 
 
-        if prefix.lower() != auth_prefix_token:
+        if prefix.lower() != auth_prefix_header:
             msg = 'El prefijo de autenticación no es el esperado.'
             raise AuthenticationFailed(msg)
 
@@ -65,39 +65,39 @@ class JWTAuthentication(BaseAuthentication):
 
 
 
-        def _authenticate_credentials(self, request, jwt_token):
-            try:
-                """ Decodificamos el token """
-                payload = jwt.decode(jwt_token, settigs.SECRET_KEY)
+    def _authenticate_credentials(self, request, jwt_token):
 
-                """ Obtenemos el tipo de token que se decodifico  """
-                token_type = payload['token']
+        try:
+            """ Decodificamos el token """
+            payload = jwt.decode(jwt_token, settings.SECRET_KEY)
 
-                """ Dependiendo del token validamos que exista en la base de datos """
-                if token_type == 'access_token':
-                    user = User.objects.get(access_token=jwt_token)
+            """ Obtenemos el tipo de token que se decodifico  """
+            token_type = payload['token']
 
-                if token_type == 'refresh_token':
-                    user = User.objects.get(refresh_token=jwt_token)
+            """ Dependiendo del token validamos que exista en la base de datos """
+            if token_type == 'access':
+                user = User.objects.get(access_token=jwt_token)
 
-                if not user.is_active:
-                    msg = 'El usuario al que intentas autenticarte se encuentra baneado del sistema.'
-                    raise AuthenticationFailed(msg)
+            if token_type == 'refresh':
+                user = User.objects.get(refresh_token=jwt_token)
 
-            except jwt.ExpiredSignatureError as e:
-                msg = 'El token de autenticación ha expirado.'
+            if not user.is_active:
+                msg = 'El usuario al que intentas autenticarte se encuentra baneado del sistema.'
                 raise AuthenticationFailed(msg)
 
-            except jwt.exceptions.DecodeError as e:
-                msg = 'Ocurrio un error al decoficar el token.'
-                raise AuthenticationFailed(msg)
+        except jwt.ExpiredSignatureError as e:
+            msg = 'El token de autenticación ha expirado.'
+            raise AuthenticationFailed(msg)
 
-            except user.DoesNotExist as e:
-                msg = 'El token de autenticación no existe o fue eliminado.'
-                raise AuthenticationFailed(msg)
+        except jwt.exceptions.DecodeError as e:
+            msg = 'Ocurrio un error al decoficar el token.'
+            raise AuthenticationFailed(msg)
 
+        except User.DoesNotExist as e:
+            msg = 'El token de autenticación no existe o fue eliminado.'
+            raise AuthenticationFailed(msg)
 
-            return (user, token)
+        return (user, jwt_token)
 
 
 
