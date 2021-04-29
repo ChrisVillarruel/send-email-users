952c595edbeb1b9180400bb4a17370fbfe156d94
chris_villarruel <givc980909@gmail.com>
2021-04-29 14:35:13 -0500

    Se implementaron los metodos get_admin y send_email

--- apps/users/api/serializers/login.py
+++ apps/users/api/serializers/login.py
@@ -1,18 +1,23 @@
-from rest_framework import serializers 
+
+# Modulos rest_framework
+from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed
 
 # Modulos nativos de django
 from django.contrib.auth import authenticate
 
+# Modulos locales
 from apps.users.models import User
 from apps.users.timezone import get_timezone
+from apps.users.send_email import send_email, get_admin
 
- 
+
+
 
 class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=200)
     password = serializers.CharField(max_length=64, min_length=8, write_only=True)
-    
+
 
     class Meta:
         model = User
@@ -22,9 +27,7 @@ class LoginSerializer(serializers.ModelSerializer):
     def validate(self, data):
         email = data.get('email', None)
         password = data.get('password', None)
-        
-        validate_account = User.objects.filter(email=email).first()
-        
+
         """
         Antes de que un usuario se autentique se debera validar algunos datos.
         1. Que la dirección de correo electronico exista.
@@ -33,32 +36,50 @@ class LoginSerializer(serializers.ModelSerializer):
 
         """
         
-        if validate_account is None:
+        user_exists = self.Meta.model.objects.filter(email=email).first()
+
+        if user_exists is None:
             msg = 'La dirección de correo elctronico que usted ingreso no existe.'
             raise AuthenticationFailed(msg)
-        
-        if not validate_account.is_active:
+
+        if not user_exists.is_active:
             msg = 'La cuenta a la que intenta iniciar sesión fue desactivada.'
             raise AuthenticationFailed(msg)
 
 
         """
-        Si las validaciones anteriores fueron superadas ahora se debe de validar.
+        Si las validaciones anteriores fueron correctas ahora se debe de validar.
+
         1. Que las credenciales de autenticación sean las correctas.
-        2. Con el metodo save, actualizamos los tokens siempre y cuando hayan caducado.
+        2. Si el usuario no ingresa bien su contraseña el sistema de manera automatica
+           enviara un correo al administrador del sistema que se le notificara que o
+           bien alguien ajeno quiere acceder o al cliente se le olvido la contraseña.
+
+           ya dependera del administrador resetear la contraseña o no.
+
+        3. Y con el metodo save, actualizamos los tokens siempre y cuando hayan caducado.
 
 
         """
+
         user = authenticate(username=email, password=password)
-         
+
+
         if user is None:
+            if user_exists.role == 'Cliente':
+                send_email(user_exists.full_name, user_exists, get_admin(self))
+ 
+                msg = 'Si se le olvido su contraseña, ya se le notifico al administrador para investigar su caso. De lo contrario intente de nuevo iniciar sesión.'
+                raise AuthenticationFailed(msg)
+
+
             msg = 'Dirección de correo electronico y/o contraseña incorrecta.'
             raise AuthenticationFailed(msg)
 
 
         user.last_login = get_timezone()
         user.save()
-        
+
 
         return {
             'email': user.email,
@@ -69,4 +90,4 @@ class LoginSerializer(serializers.ModelSerializer):
 
 
 
-    
+
