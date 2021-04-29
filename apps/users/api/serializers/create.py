66894c130fe33980b3207941a6d39207078edc4b
chris_villarruel <givc980909@gmail.com>
2021-04-29 14:32:20 -0500

    Seañadieron nuevas validaciones dentro del modulo serializers/create.py

--- apps/users/api/serializers/create.py
+++ apps/users/api/serializers/create.py
@@ -1,17 +1,28 @@
+
+# modulos de rest_framework
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+# Modulos locales
 from apps.users.models import User
 
+
+""" Validamo que la dirección de correo electronico no exista  """
 duplicate_user = UniqueValidator(queryset=User.objects.all().values('email'),
         message='La dirección de correo electronico que usted ingreso ya existe.')
 
 
+def test(value):
+    if value.isnumeric():
+        msg = 'Los caracteres numericos no son validos dentro de este campo.'
+        raise serializers.ValidationError(msg)
+    
+
 
 class CreateUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(max_length=200, validators=[duplicate_user])
-    full_name = serializers.CharField(max_length=200)
-    description = serializers.CharField(max_length=250)
+    full_name = serializers.CharField(max_length=200, validators=[test])
+    description = serializers.CharField(max_length=250, validators=[test])
     password = serializers.CharField(max_length=64, min_length=8, write_only=True)
 
     class Meta:
@@ -19,8 +30,6 @@ class CreateUserSerializer(serializers.ModelSerializer):
         fields = ['email', 'full_name', 'description', 'password', 'role', 'token']
         read_only_fields = ('role', 'token')
 
-    def validate(self, data):
-        return data
-
+    
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
